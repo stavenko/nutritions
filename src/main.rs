8f4cb2fd5be2c7e90249ef27f3d37487add9b07b
Ставенко Василий <vgstav10@mts.ru>
2023-02-22 17:35:22 +0300

    Add links to other recipes

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
+use std::{error::Error, path::PathBuf};
+
 use clap::Parser;
 use recipe::Recipe;
-use tokio::io::AsyncReadExt;
 mod recipe;
 
 #[derive(clap::Parser)]
 struct Opts {
     #[clap(long, short)]
-    recipe_file: String,
+    recipe_file: PathBuf,
 }
 
-async fn cli() {
+async fn cli() -> Result<(), Box<dyn Error>> {
     let opts = Opts::parse();
-    let mut file = tokio::fs::File::open(&opts.recipe_file)
-        .await
-        .unwrap_or_else(|_| panic!("File {} expected to be", opts.recipe_file));
-    let mut file_contents: String = "".into();
-    file.read_to_string(&mut file_contents).await.unwrap();
-    let recipe: Recipe = serde_yaml::from_str(&file_contents).unwrap();
-    let facts = recipe.get_nutrition_facts();
+    // let mut file = tokio::fs::File::open(&opts.recipe_file)
+    // .await
+    // .unwrap_or_else(|_| panic!("File {} expected to be", opts.recipe_file));
+    // let mut file_contents: String = "".into();
+    // file.read_to_string(&mut file_contents).await.unwrap();
+    let recipe = Recipe::read_from_file(&opts.recipe_file).await?;
+    let facts = recipe.get_nutrition_facts().await?;
 
-    println!("Facts: {}\n{}", opts.recipe_file, facts);
+    println!("Facts: {}\n{}", opts.recipe_file.to_string_lossy(), facts);
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
-    cli().await;
+    cli().await.unwrap();
 }
--- src/recipe.rs
+++ src/recipe.rs
@@ -1,8 +1,34 @@
+use async_recursion::async_recursion;
 use core::fmt;
 use enum_iterator::Sequence;
 use serde::Deserialize;
-use std::{collections::HashMap, fmt::Write};
+use std::{
+    collections::HashMap,
+    error::Error,
+    fmt::Write,
+    path::{Path, PathBuf},
+};
+use tokio::io::AsyncReadExt;
 
+#[derive(Clone, Default, Debug, PartialEq, Deserialize)]
+pub struct NutritionFacts(HashMap<Nutrition, f64>);
+
+impl NutritionFacts {
+    fn into_inner(self) -> HashMap<Nutrition, f64> {
+        self.0
+    }
+}
+
+impl fmt::Display for NutritionFacts {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for item in enum_iterator::all::<Nutrition>() {
+            if let Some(value) = self.0.get(&item) {
+                write!(f, "{:?}:  {:.2}\n", item, value)?;
+            }
+        }
+        Ok(())
+    }
+}
 #[derive(Deserialize)]
 pub struct Recipe {
     products: Vec<Product>,
@@ -24,7 +50,27 @@ pub struct Ingredient {
 #[derive(Deserialize)]
 pub struct Product {
     name: String,
-    facts: HashMap<Nutrition, f64>,
+    #[serde(flatten)]
+    nutrition_data: NutritionData,
+}
+
+impl Product {
+    async fn get_nutrition_facts(&self) -> Result<NutritionFacts, Box<dyn Error>> {
+        match self.nutrition_data {
+            NutritionData::Facts(ref facts) => Ok(facts.clone()),
+            NutritionData::Recipe(ref path) => {
+                let recipe = Recipe::read_from_file(&path).await?;
+                recipe.get_nutrition_facts().await
+            }
+        }
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum NutritionData {
+    Facts(NutritionFacts),
+    Recipe(PathBuf),
 }
 
 #[derive(Deserialize, PartialEq, Eq, Hash, Sequence, Debug, Clone, Copy)]
@@ -35,30 +81,33 @@ pub enum Nutrition {
     Carbohydrates,
 }
 
-#[derive(Default, Debug, PartialEq)]
-pub struct NutritionFacts(HashMap<Nutrition, f64>);
-
-impl fmt::Display for NutritionFacts {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for item in enum_iterator::all::<Nutrition>() {
-            if let Some(value) = self.0.get(&item) {
-                write!(f, "{:?}:  {:.2}\n", item, value)?;
-            }
-        }
-        Ok(())
-    }
-}
-
 impl Recipe {
-    pub fn get_nutrition_facts(&self) -> NutritionFacts {
+    pub async fn read_from_file(path: &Path) -> Result<Self, Box<dyn Error>> {
+        let mut file = tokio::fs::File::open(path).await?;
+
+        let mut file_contents: String = "".into();
+        file.read_to_string(&mut file_contents).await?;
+        let recipe: Recipe = serde_yaml::from_str(&file_contents)?;
+        Ok(recipe)
+    }
+
+    #[async_recursion]
+    pub async fn get_nutrition_facts(&self) -> Result<NutritionFacts, Box<dyn Error>> {
         let mut totals_for_dish: HashMap<Nutrition, f64> = HashMap::new();
         let mut total_ingredients_weight = 0.0;
         for ingredient in &self.dish.ingredients {
             if let Some(product) = self.products.iter().find(|p| p.name == ingredient.product) {
                 total_ingredients_weight += ingredient.amount;
-                for (nutrient, amount) in &product.facts {
-                    let this_amount = amount/100.0 * ingredient.amount;
-                    println!("add {} {:?} {:?} {}g = {}",product.name, nutrient, amount/100.0, ingredient.amount, this_amount);
+                for (nutrient, amount) in &product.get_nutrition_facts().await?.into_inner() {
+                    let this_amount = amount / 100.0 * ingredient.amount;
+                    println!(
+                        "add {} {:?} {:?} {}g = {}",
+                        product.name,
+                        nutrient,
+                        amount / 100.0,
+                        ingredient.amount,
+                        this_amount
+                    );
                     totals_for_dish
                         .entry(*nutrient)
                         .and_modify(|v| *v += this_amount)
@@ -68,30 +117,45 @@ impl Recipe {
                 panic!(
                     "Cannot find ingredient in recipe: {} possible products: {}",
                     ingredient.product,
-                    self.products.iter()
-                    .map(|p| p.name.clone())
-                    .collect::<Vec<_>>()
-                    .join(", ")
+                    self.products
+                        .iter()
+                        .map(|p| p.name.clone())
+                        .collect::<Vec<_>>()
+                        .join(", ")
                 )
             }
         }
-        println!("Totals for raw ingredients {:?} {}", totals_for_dish, total_ingredients_weight);
+        println!(
+            "Totals for raw ingredients {:?} {}",
+            totals_for_dish, total_ingredients_weight
+        );
 
-        let ing_coef = self.dish.weight.map(|dish_weight| total_ingredients_weight / dish_weight).unwrap_or(1.0);
+        let ing_coef = self
+            .dish
+            .weight
+            .map(|dish_weight| total_ingredients_weight / dish_weight)
+            .unwrap_or(1.0);
         println!("{}", ing_coef);
 
-        println!("whole result dish {:?}",    totals_for_dish
+        println!(
+            "whole result dish {:?}",
+            totals_for_dish
                 .iter()
                 .map(|(k, a)| (k, a * ing_coef))
-                .collect::<Vec<_>>());
-        let weight_to_hundred = self.dish.weight.map(|dish_weight| 100.0 / dish_weight).unwrap_or(100.0 / total_ingredients_weight);
+                .collect::<Vec<_>>()
+        );
+        let weight_to_hundred = self
+            .dish
+            .weight
+            .map(|dish_weight| 100.0 / dish_weight)
+            .unwrap_or(100.0 / total_ingredients_weight);
 
-        NutritionFacts(
+        Ok(NutritionFacts(
             totals_for_dish
                 .into_iter()
                 .map(|(k, a)| (k, a * ing_coef * weight_to_hundred))
                 .collect(),
-        )
+        ))
     }
 }
 
@@ -124,7 +188,9 @@ mod test {
         )
     }
     #[test]
-    #[should_panic(expected= "Cannot find ingredient in recipe: cabbage possible products: Oil, Milk")]
+    #[should_panic(
+        expected = "Cannot find ingredient in recipe: cabbage possible products: Oil, Milk"
+    )]
     fn fail_not_found() {
         let milk = Product {
             name: "Milk".into(),
